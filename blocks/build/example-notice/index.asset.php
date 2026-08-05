f08e152c85e260bdb1696202c65d43e8288db9f1
Tony Hegyes <a.hegyes@proton.me>
2026-08-05 10:45:12 +0200

    regenerate assets

--- blocks/build/example-notice/index.asset.php
+++ blocks/build/example-notice/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6573fb3a2c624d523e45');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '558bb36788171a7c90b5');
